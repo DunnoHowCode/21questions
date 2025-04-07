86cfcb1ee4fa239e5ebf08acbb1222ee4f164ed1
DunnoHowCode <ljsmith9@cps.edu>
2025-04-07 15:56:49 +0000

    working on background

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let dog = document.querySelector(".dog");
 let bat = document.querySelector(".bat");
 let barn = document.querySelector(".barn");
 let nest = document.querySelector(".nest");
-dog.style.display = "none"
-bat.style.display = "none"
-barn.style.display = "none"
-nest.style.display = "none"
+// dog.style.display = "none"
+// bat.style.display = "none"
+// barn.style.display = "none"
+// nest.style.display = "none"
 
 results.addEventListener('click', function(){
     console.log("Scadooooooosh");
