ec44ba1257223fbc9610e452aacc05a0499bcb90
DunnoHowCode <ljsmith9@cps.edu>
2025-04-13 22:39:02 +0000

    results

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let mood = document.querySelector(".mood");
 let number = document.querySelector(".number");
 let results = document.querySelector(".results");
 let header = document.querySelector(".header");
+let input = document.querySelector("input");
+let answer = document.querySelector("h1");
 console.log(mood);
 console.log(number);
 console.log(results);
@@ -10,10 +12,12 @@ let dog = document.querySelector(".dog");
 let bat = document.querySelector(".bat");
 let barn = document.querySelector(".barn");
 let nest = document.querySelector(".nest");
-dog.style.display = "none"
-bat.style.display = "none"
-barn.style.display = "none"
-nest.style.display = "none"
+let duck = document.querySelector(".duck");
+dog.style.display = "none";
+bat.style.display = "none";
+barn.style.display = "none";
+nest.style.display = "none";
+duck.style.display = "block";
 
 results.addEventListener('click', function(){
     console.log("Scadooooooosh");
@@ -47,15 +51,31 @@ console.log(x);
 }
 
    if(x === true && mood === "Happy"){
-     barn.style.display = "block";
+    answer.innerHTML = "Your word is...";
+    duck.style.display = "none";
+    input.style.display = "none";
+    results.style.display = "none";
+    barn.style.display = "block";
   }
   if(x === true && mood === "Sad"){
+    answer.innerHTML = "Your word is...";
+    duck.style.display = "none";
+    input.style.display = "none";
+    results.style.display = "none";
     nest.style.display = "block";
   }
   if(x === false && mood === "Happy"){
+    answer.innerHTML = "Your word is...";
+    duck.style.display = "none";
+    input.style.display = "none";
+    results.style.display = "none";
     dog.style.display = "block";
   }
   if(x === false && mood === "Sad"){
+    answer.innerHTML = "Your word is...";
+    duck.style.display = "none";
+    input.style.display = "none";
+    results.style.display = "none";
     bat.style.display = "block";
   }
     
